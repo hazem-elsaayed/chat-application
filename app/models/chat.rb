7fb1d7992182ed09522b5f38290416f3b734b8ee
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-07 20:43:02 +0300

    refactor: Update Chat model associations and validations

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,6 @@
 class Chat < ApplicationRecord
-  belongs_to :application
+  belongs_to :application, class_name: "Application", foreign_key: "application_id"
+  has_many :message, class_name: "Message", foreign_key: "chat_id"
+  validates :name, :chat_number, presence: true
+  validates :chat_number, uniqueness: true
 end
