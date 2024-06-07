e5fba960cf1f09d73cb42a6e609ba5a6b6ebf5b1
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-07 20:41:51 +0300

    feat: Add validations and relation to application table

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,13 @@
 class Application < ApplicationRecord
+  has_many :chat, class_name: "Chat", foreign_key: "application_id"
+  validates :name, presence: true
+  validates :token, uniqueness: true
+  before_create :generate_token
+
+  def generate_token(length=10)
+    loop do
+      token = self.token = SecureRandom.urlsafe_base64(length, false)
+      break token unless self.class.exists?(token: token)
+    end
+  end
 end
