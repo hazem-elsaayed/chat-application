7fdad37970856165fad71f03ef84ff7bfe95015d
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-07 20:45:30 +0300

    refactor: Update Message model validations and associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
-  belongs_to :chat
+  validates :sender, :content, :message_number, presence: true
+  validates :message_number, uniqueness: true
+  belongs_to :chat, class_name: 'Chat', foreign_key: 'chat_id'
 end
