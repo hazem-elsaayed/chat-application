cc667bccb0b99b70e36058414940d45570787e4b
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-07 23:12:00 +0300

    feat: Add route_not_found method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def route_not_found
+    render json: {success: false, message: 'request url not found'}, status: :not_found, layout: false
+  end    
 end
