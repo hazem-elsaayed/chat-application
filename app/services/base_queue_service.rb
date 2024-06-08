e904668cf278f14767045c935e52a4a0bdf3ca40
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-08 15:12:53 +0300

    chore: Update BaseQueueService to use environment variable for RabbitMQ host

--- app/services/base_queue_service.rb
+++ app/services/base_queue_service.rb
@@ -5,14 +5,14 @@ class BaseQueueService
   attr_accessor :connection, :channel
 
   def initialize
-    @connection = Bunny.new(automatically_recover: true, host: 'localhost')
+    return if ENV['DISABLE_RABBITMQ']
+    @connection = Bunny.new(automatically_recover: true, host: ENV.fetch('RABBITMQ_HOST'))
     @connection.start
     @channel = @connection.create_channel
     channel
   end
 
   def close
-    @connection.close
+    @connection.close unless ENV['DISABLE_RABBITMQ']
   end
-
 end
