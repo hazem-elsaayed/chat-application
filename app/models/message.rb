d44e1186e3269c57a78ebfff8803153cb574f95b
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-08 15:12:23 +0300

    chore: Include Elasticsearch integration for Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,38 @@
 class Message < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name Rails.application.class.module_parent_name.underscore
+
   validates :sender, :content, :message_number, presence: true
-  validates :message_number, uniqueness: true
   belongs_to :chat, class_name: 'Chat', foreign_key: 'chat_id'
+
+  settings index: { number_of_shards: 1 } do
+    mapping dynamic: false do
+      indexes :sender, type: :text, analyzer: 'english'
+      indexes :content, type: :text, analyzer: 'english'
+    end
+  end
+
+  def self.search(query, chat_id)
+    __elasticsearch__.search(
+      {
+        query: {
+          bool: {
+            must: {
+              query_string: {
+                query: "*#{query}*",
+                fields: ['content^5', 'sender']
+              }
+            },
+            filter: {
+              term: {
+                chat_id: chat_id
+              }
+            }
+          }
+        }
+      }
+    )
+  end
 end
