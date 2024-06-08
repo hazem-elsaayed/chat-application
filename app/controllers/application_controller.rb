57ee706baf12c621fc109bb74531779bf6595d5e
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-08 15:12:07 +0300

    feat: Add protection against CSRF attacks in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
   def route_not_found
     render json: {success: false, message: 'request url not found'}, status: :not_found, layout: false
   end    
