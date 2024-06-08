c517208414723cb797798a9d7ec9b0216bbef650
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-06-08 15:12:58 +0300

    feat: Add conditional check to ConsumerService to disable RabbitMQ if environment variable is set

--- app/services/consumer_service.rb
+++ app/services/consumer_service.rb
@@ -1,6 +1,7 @@
 class ConsumerService
 
   def initialize
+    return if ENV['DISABLE_RABBITMQ']
     @mq_service = BaseQueueService.instance
     @channel = @mq_service.channel
     @channel.prefetch(1)
@@ -8,14 +9,16 @@ class ConsumerService
   end
 
   def consume_message(queue_name, model)
+    return if ENV['DISABLE_RABBITMQ']
     queue = @channel.queue(queue_name, durable: true)
-    puts 'Start Consuming Message'
+    puts "Start Consuming Message for #{model.name}"
     begin
       queue.subscribe(manual_ack: true) do |delivery_info, _properties, payload|
         puts " [x] Received '#{payload}'"
         msg = JSON.parse(payload)
         model.create!(msg)
-        channel.ack(delivery_info.delivery_tag)
+        @channel.ack(delivery_info.delivery_tag)
+        puts " [x] Acknowledged '#{payload}'"
       end
     rescue StandardError => e
       puts e
